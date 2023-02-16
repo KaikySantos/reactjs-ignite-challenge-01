13b410d133d0f46f253dbe15461e659a04f66cc5
kaikySantos <info.kaiky@gmail.com>
2023-02-16 15:46:36 -0300

    fix: header image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,9 @@ export function Header() {
   return (
     <header className="header">
       <div>
-        <img src="/logo.svg" alt="to.do"/>
+        <h1>
+          to.<span>do</span>
+        </h1>
       </div>
     </header>
   )
